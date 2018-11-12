dd34ff242b097e364a56158879db084d7cbfecb0
DonCarltons <44951725+DonCarltons@users.noreply.github.com>
2018-11-11 16:26:03 -0800

    Add files via upload
    Changed monster Stats

---
+++ GiantSpider.java
@@ -0,0 +1,46 @@
+/**
+ * ---------------------------------------------------------------------------
+ * File name: GiantSpider.java
+ * Project name: 1260-001-TranThomas-CarltonRobert-Project5
+ * ---------------------------------------------------------------------------
+ * Creator's name and email: Carlton Robert, Carltonr@etsu.edu
+ * Course:  CSCI 1260
+ * Creation Date: Nov 11, 2018
+ * ---------------------------------------------------------------------------
+ */
+package entity;
+
+/**
+ * 
+ * Giant spider to attack player
+ *
+ * <hr>
+ * Date created: Nov 11, 2018
+ * <hr>
+ * @author Carlton Robert
+ */
+public class GiantSpider extends Monster {
+
+	/**
+	 * 
+	 * Base Constructor        
+	 *
+	 * <hr>
+	 * Date created: Nov 11, 2018 
+	 *
+	 * 
+	 * @param health
+	 * @param damage
+	 * @param accuracy
+	 * @param name
+	 */
+	public GiantSpider() {
+
+		super();
+		name = "Giant Spider";
+		this.damage = 15;
+		this.health = 60;
+		this.accuracy = 0.45;
+	}
+
+}
