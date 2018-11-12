c0570cf25ad23555b5836efe53a2085cddab4f9c
DonCarltons <44951725+DonCarltons@users.noreply.github.com>
2018-11-11 16:26:57 -0800

    Add files via upload

---
+++ Fist.java
@@ -0,0 +1,44 @@
+/**
+ * ---------------------------------------------------------------------------
+ * File name: Fist.java
+ * Project name: 1260-001-TranThomas-CarltonRobert-Project5
+ * ---------------------------------------------------------------------------
+ * Creator's name and email: Carlton Robert, Carltonr@etsu.edu
+ * Course:  CSCI 1260
+ * Creation Date: Nov 11, 2018
+ * ---------------------------------------------------------------------------
+ */
+package weapon;
+
+/**
+ * 
+ * Basic weapon player will always have
+ *
+ * <hr>
+ * Date created: Nov 11, 2018
+ * <hr>
+ * @author Carlton Robert
+ */
+public class Fist extends Weapon {
+
+	/**
+	 * 
+	 * Constructor        
+	 *
+	 * <hr>
+	 * Date created: Nov 11, 2018 
+	 *
+	 *
+	 */
+	public Fist() {
+		// TODO Auto-generated constructor stub
+		super();
+		name = "fist";
+		damage = 5;
+		accuracy = 0.10;
+		
+	}
+
+	
+
+}
